209d67fea9777c094876a9d84640e270b3274cef
aidanaden <aidan@u.nus.edu>
2024-01-18 07:33:26 +0800

    fix: remove example onmount

--- dev/src/app.tsx
+++ dev/src/app.tsx
@@ -1,32 +1,21 @@
-import { Component, createMemo, createSignal, onMount } from "solid-js";
+import { Component } from "solid-js";
 import { PythonProvider } from "solid-py";
 
 import CodeEditor from "./components/code-editor";
 import "./main.css";
 
 export const Example: Component = () => {
-  const [searchText, setSearchText] = createSignal("and or the");
-  const [textToHighlight, setTextToHighlight] = createSignal(
-    `When in the Course of human events it becomes necessary for one people to dissolve the political bands which have connected them with another and to assume among the powers of the earth, the separate and equal station to which the Laws of Nature and of Nature's God entitle them, a decent respect to the opinions of mankind requires that they should declare the causes which impel them to the separation.`
-  );
-  const [activeIdx, setActiveIdx] = createSignal(-1);
-  const [caseSensitive, setCaseSensitive] = createSignal(false);
-  const searchWords = createMemo(() =>
-    searchText()
-      .split(/\s/)
-      .filter((word) => word)
-  );
-  onMount(() => {
-    navigator.serviceWorker
-      .register("/solid-py-sw.ts")
-      .then((registration) =>
-        console.log(
-          "Service Worker registration successful with scope: ",
-          registration.scope
-        )
-      )
-      .catch((err) => console.log("Service Worker registration failed: ", err));
-  });
+  // onMount(() => {
+  //   navigator.serviceWorker
+  //     .register("/solid-py-sw.ts")
+  //     .then((registration) =>
+  //       console.log(
+  //         "Service Worker registration successful with scope: ",
+  //         registration.scope
+  //       )
+  //     )
+  //     .catch((err) => console.log("Service Worker registration failed: ", err));
+  // });
   return (
     <PythonProvider>
       <div class="p-3">
