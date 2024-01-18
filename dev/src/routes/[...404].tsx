6dd98b1cb061ce0fb482d80645f7485968d3b7cf
aidanaden <aidan@u.nus.edu>
2024-01-19 04:27:00 +0800

    test: deploy 2

--- dev/src/routes/[...404].tsx
+++ dev/src/routes/[...404].tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <main class="text-center mx-auto text-gray-700 p-4">
       <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
-        Not Found
+        Not Found!
       </h1>
       <p class="mt-8">
         Visit{" "}
